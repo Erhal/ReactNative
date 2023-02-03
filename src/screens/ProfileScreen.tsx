0ca57e9d134fcf95db37782ec33a84aa9f89626a
Erhal <odhernes@gmail.com>
2023-02-03 11:18:15 +0400

    Update ProfileScreen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,11 +1,23 @@
-import { SafeAreaView, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const ProfileScreen = () => {
     return (
-        <SafeAreaView>
-            <Text>Test page</Text>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <Text style={styles.text}>Account Page</Text>
+        </View>
     );
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#0A0F14",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        color: "#fff",
+    }
+});
